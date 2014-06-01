aee6222480448a4da75313ff7b5de9ad597814aa
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-06-01 11:52:47 +0100

    Jquery theme not working with form

--- app/assets/javascripts/jquery.js
+++ app/assets/javascripts/jquery.js
@@ -2,9 +2,27 @@ $(document).ready(function(){
   alert('jquery is in the house')
 $('#showAlbums').on('click',function(){
     $('#albumDropdown').slideToggle();
-  })
+  });
 
+$('#showSongs').on('click',function(){
+    $('#songDropdown').slideToggle();
+  });
 
+// $("#theme").val('Purple',function(){
+//   alert('purple');
+// });
 
+$('#red').on('click', function(){
+  $('head').append('<%= stylesheet_link_tag "themes/angry.css", :media => "all" %>');
+  
+});
+//
+// $('select>option:eq(1)').prop('selected', true ,function(){
+  
+//   $('head').append('<link rel="stylesheet" type="text/css" href="themes/angry.css">');
+// });
+
+  
 
 })
+
