643d42e348955081422931ca2f7d7875a597ab6b
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-28 13:10:47 +0100

    commeted out js file

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,35 +1,35 @@
-$(function(){
-  $('form').submit(function(e){
-    e.preventDefault();
-    var searchString = encodeURI($("input[name=m]").val());
-    var requestUrl = 'http://ws.spotify.com/search/1/track.json?s=' + searchString;
-    $.ajax({
-      type: "GET", // get request pass arugments directly into the URI
-      url: requestUrl,
-      dataType: "json"
-    }).done(populateSearch) // some knows we have 
-    return false; // double varication...
-  });
+// $(function(){
+//   $('form').submit(function(e){
+//     e.preventDefault();
+//     var searchString = encodeURI($("input[name=m]").val());
+//     var requestUrl = 'http://ws.spotify.com/search/1/track.json?s=' + searchString;
+//     $.ajax({
+//       type: "GET", // get request pass arugments directly into the URI
+//       url: requestUrl,
+//       dataType: "json"
+//     }).done(populateSearch) // some knows we have 
+//     return false; // double varication...
+//   });
 
 
-  function populateSearch(data){
-  // example contents of data
-  // data = {"Search":[{"Title":"Die Hard","Year":"1988","imdbID":"tt0095016","Type":"movie"}, ...]}
-  // accessing the key - saying if this key is undefined something wrong happend
-  if(data.Search != undefined){
-    $("input[name=m]").val("");
-    $("#results").empty();
+//   function populateSearch(data){
+//   // example contents of data
+//   // data = {"Search":[{"Title":"Die Hard","Year":"1988","imdbID":"tt0095016","Type":"movie"}, ...]}
+//   // accessing the key - saying if this key is undefined something wrong happend
+//   if(data.Search != undefined){
+//     $("input[name=m]").val("");
+//     $("#results").empty();
 
-    $.each(data.Search, function(i,song){
-      html_string = $("<li><a class='movie-title' data-id='"+movie.imdbID+"'>"+movie.
-      Title+"</a>("+movie.Year+")</li>"); 
-      html_string = $("<li>" + song.name + "<a class='song-link' href='"+song.href+"'>"+"link to sptotfy"+
-        "</a>("+track.track-number+")</li>");
-             // the data id put the imdbid inside!
-      $("#results").append(html_string)
-    })
-  }else{
-    alert("something wetn wrong");
-  }
+//     $.each(data.Search, function(i,song){
+//       html_string = $("<li><a class='movie-title' data-id='"+movie.imdbID+"'>"+movie.
+//       Title+"</a>("+movie.Year+")</li>"); 
+//       html_string = $("<li>" + song.name + "<a class='song-link' href='"+song.href+"'>"+"link to sptotfy"+
+//         "</a>("+track.track-number+")</li>");
+//              // the data id put the imdbid inside!
+//       $("#results").append(html_string)
+//     })
+//   }else{
+//     alert("something wetn wrong");
+//   }
 
 
--- app/assets/javascripts/themes.js
+++ app/assets/javascripts/themes.js
@@ -1,37 +1,37 @@
-$(document).ready(function(){
+// $(document).ready(function(){
   
-$('#showAlbums').on('click',function(){
-    $('#albumDropdown').slideToggle('slow','linear');
-  });
+// $('#showAlbums').on('click',function(){
+//     $('#albumDropdown').slideToggle('slow','linear');
+//   });
 
-$('#showSongs').on('click',function(){
-    $('#songDropdown').slideToggle('slow','linear');
-  });
+// $('#showSongs').on('click',function(){
+//     $('#songDropdown').slideToggle('slow','linear');
+//   });
 
 
 
-// $("#theme").val('Purple',function(){
-//   alert('purple');
+// // $("#theme").val('Purple',function(){
+// //   alert('purple');
+// // });
+//       // too test if hte background actually works
+// $("select").change(function(){
+//   var str = "";
+//   $("select option:selected" ).each(function() {
+//       str += $( this ).val().toLowerCase();
+//   });
+//   url = '<link href="/assets/themes/'+ str +'.css?body=1" rel="stylesheet" type="text/css" media="all">'
+//   console.log(url)
+//   $('head').append(url);
 // });
-      // too test if hte background actually works
-$("select").change(function(){
-  var str = "";
-  $("select option:selected" ).each(function() {
-      str += $( this ).val().toLowerCase();
-  });
-  url = '<link href="/assets/themes/'+ str +'.css?body=1" rel="stylesheet" type="text/css" media="all">'
-  console.log(url)
-  $('head').append(url);
-});
-$('head').append('<link href="/assets/themes/'+ str +'.css?body=1" rel="stylesheet" type="text/css" media="all">');
+// $('head').append('<link href="/assets/themes/'+ str +'.css?body=1" rel="stylesheet" type="text/css" media="all">');
 
 
 
-$('#red').on('click', function(){
-  $('head').append('<link href="/assets/themes/angry.css?body=1" media="all" rel="stylesheet" type="text/css">');
-  // how to i add this!!!! i want external stylesheet to link
-});
-//    // this is for the select actuall select drop down. 
+// $('#red').on('click', function(){
+//   $('head').append('<link href="/assets/themes/angry.css?body=1" media="all" rel="stylesheet" type="text/css">');
+//   // how to i add this!!!! i want external stylesheet to link
+// });
+// //    // this is for the select actuall select drop down. 
 
 
   
